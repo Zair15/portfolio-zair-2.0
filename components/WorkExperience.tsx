b8333aed09941bf9240aff1cf5fba8afd8417dd3
Zair15 <zairabbas1533@outlook.com>
2022-11-10 18:15:38 +0500

    design updated

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -12,10 +12,10 @@ export default function WorkExperience({}: Props) {
       transition={{ duration: 1.5 }}
       className="relative flex flex-col items-center h-screen px-10 mx-auto text-center md:text-left md:flex-row max-w-7xl md:justify-evenly"
     >
-      <h3 className="absolute uppercase top-24 tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute uppercase mt-20 md:mt-0 md:top-24 xl:top-9 tracking-[15px] text-gray-500 text-1xl md:text-2xl">
         Experience
       </h3>
-      <div className="flex w-full p-10 mt-10 space-x-5 overflow-x-scroll md:mt-0 snap-x snap-mandatory">
+      <div className="flex w-full p-8 mt-24 space-x-5 overflow-x-scroll md:mt-36 snap-x snap-mandatory">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
